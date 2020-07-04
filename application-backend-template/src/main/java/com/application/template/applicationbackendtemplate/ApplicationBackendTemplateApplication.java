5535fd32042ba1021e0d1974ea60e109bffc3f5e
vishnitin <vishwanath119@gmail.com>
2020-07-05 02:33:40 +0530

    Adding Spring Boot Backend Project

---
+++ application-backend-template/src/main/java/com/application/template/applicationbackendtemplate/ApplicationBackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.application.template.applicationbackendtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBackendTemplateApplication.class, args);
+	}
+
+}
---
+++ application-backend-template/src/test/java/com/application/template/applicationbackendtemplate/ApplicationBackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.template.applicationbackendtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
